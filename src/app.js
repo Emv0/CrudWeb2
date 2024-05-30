4e2aa14ae27a6c6cb9053c4ffc3ad0ad0171cf14
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-05-29 21:07:05 -0500

    Se crearon nuevas carpetas del back

--- src/app.js
+++ src/app.js
@@ -7,11 +7,27 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('port', 4000);
 
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json());
+app.engine('.hbs',engine({
+    extname:'.hbs'
+}));
+app.set('view engine','hbs');
+
+app.set('view',__dirname+'/views');
+
 app.use(myConnection(mysql,{
     host:"localhost",
     user:"root",
     password:"",
     port:3306,
-    database:"movies"
+    database:"crudnodejs"
 },'single'));
 
+app.listen(app.get('port'),()=>{
+    console.log('Listening on port', app.get('port'));
+});
+
+app.use('/',moviesRoutes);
